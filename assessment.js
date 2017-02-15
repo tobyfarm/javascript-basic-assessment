60be61fd8213c7e4c133c5e564341a0475285fcb
Toby Farmer <Toby@T-Macbook-13in.local>
2017-02-15 16:39:16 -0600

    poor attempt at javascript test

--- assessment.js
+++ assessment.js
@@ -1,10 +1,11 @@
 
 // #1 Create a variable called hello and assign it the string 'goodbye'
 
-
+var hello = "goodbye";
 
 // #2 Use the variable iLove to create a new variable called iLoveCode that is assigned the string "I love code"
 var iLove = 'I love';
+var iLoveCode = "I love code";
 
 // #3  Make an object called bob and give it the following properties
 // bob has a height of 6ft (string)
@@ -12,63 +13,105 @@ var iLove = 'I love';
 // bob has hair, that has style spikey, and color brown (object)
 // bob is not presidentOfTheUnitedStates (boolean)
 // bob likes apples, bananas, and cherries (array of strings)
+var bob = {
+					height: "6ft",
+					age: 24,
+					presidentOfTheUnitedStates: false,
+					likes: ["apples", "bananas", "cherries"],
+					hair: {color: "brown",
+								  style: "spikey",
+								},
+
+				};
 
 
 // #4 Change my shirt color to pink using dot notation
 var myShirt = {
 	type: 'polo',
-	color: 'red'
+	// color: 'red'
 };
+myShirt.color = "pink";
+
 
 // Change my shirt type to spandex using square bracket notation
 var myOtherShirt = {
-	type: 'polo',
+	// type: 'polo',
 	color: 'red'
 };
+myOtherShirt['type'] = 'spandex';
+
 
 // #5 Create an object that tracks a count of animals in a zoo.  Call it 'zoo'
 // The key should be the animal name(string) and the value should be how many there are.
 // Our zoo has 8 monkeys, 4 giraffes and 2 elephants
 
+var zoo = {
+	 monkeys: 8,
+	 giraffes: 4,
+	 elephants: 2,
+};
+
 
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
 var snake = {
-	sliters: 'sideways',
+	sliters: 's',
 	eats: 'rodents',
-	says: 'ssss',
-	smells: 'heat',
+	says: 's',
+	smells: 's',
 	runs: 'legless'
 };
 
 //#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
 
+var rainbowColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
 // #8 Using this array do the following
 var heroes = ['superman', 'batman', 'flash'];
 // add 'wonderwoman' to the end
+heroes.push("wonderwoman");
+var spliceOut = heroes.splice(0, 1);
+var firstHero = spliceOut.join("");
 
 // remove 'superman' and store him in a variable called firstHero
-
 // add 'spongebob' to the start of the array
-
+heroes.unshift('spongebob');
+// array is spongebob, batman, flash, wonderwoman
 // remove 'flash' from the array and store him in a variable called secondHero
+var spliceOut2 = heroes.splice(1, 1);
+var secondHero = spliceOut.join("");
 
+//  array is spongebob, batman, wonderwoman
 // leave batman in the array but put a copy of him on a variable called thirdHero
+var sliceOut = heroes.slice(0);
+var thirdHero = sliceOut.join(" ");
 
 
 
+// #9 Write a function called addItem that takes in an array and an item, adds the item to
+// the array, and returns the array with the added item.
 
-// #9 Write a function called addItem that takes in an array, an item, and adds the item to
-// the array.
-
+function addItem(arr, item){
+	arr.push(item)
+	return arr;
+}
 
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
+
+function removeItem(arr, str){
+arr.splice()
+
+}
 
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
 
+
+
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
+
+
 
 
 
@@ -94,4 +137,3 @@ var heroes = ['superman', 'batman', 'flash'];
 
 // #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
 // example - If I call you with 'Give it' you should invoke holla with 'Give it back'
-
