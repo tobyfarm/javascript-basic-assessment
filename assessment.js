e9ffced559093deffa177525b889a42bc8747b3f
Toby Farmer <Toby@T-Macbook-13in.local>
2017-02-15 22:48:43 -0600

    E for Effort

--- assessment.js
+++ assessment.js
@@ -55,12 +55,15 @@ var zoo = {
 // #6 Loop through this object and change all keys that start with the letter s to have a value of 's'
 
 var snake = {
-	sliters: 's',
+	sliters: 'bob',
 	eats: 'rodents',
-	says: 's',
-	smells: 's',
+	says: 'sssss',
+	smells: 'stinky',
 	runs: 'legless'
 };
+for(var prop in snake){
+if(prop.charAt(0) === 's'){snake[prop] = 's'};
+}
 
 //#7 Create an array of strings that are the 7 primary colors in the rainbow - red, orange, yellow, green, blue, indigo, violet (lower-case). Call your array rainbowColors
 
@@ -99,9 +102,17 @@ function addItem(arr, item){
 // #10 Write a function called removeItem that takes in an array of strings, and a string.
 
 function removeItem(arr, str){
-arr.splice()
-
+return arr.filter(function(element){
+	if(str !== element) return true;
+})
 }
+
+//  alternatively you could do str !== element
+// also Arrow Method,
+// function removeItem(arr, str){
+// return arr.filter(element => element!==str);
+// if you had an if you would do ?return bum : return sum ( SEE ARROW FUNCTIONS MDN)
+// }
 
 // Removes all instances of that string from the array. And return the modified array.
 // The order of the array should not be changed
@@ -111,6 +122,15 @@ arr.splice()
 // #11 Write a function  called doubleTheFun that takes 1 parameter. It should double numbers, and
 // repeats strings. example 4->8, 2.5->5, 'Awesome'->'AwesomeAwesome'
 
+function doubleTheFun (par){
+if(isNaN(par)){
+	return par + par
+}
+	else { return parseFloat(par) + parseFloat(par)
+
+	}
+
+}
 
 
 
@@ -118,12 +138,40 @@ arr.splice()
 // #12 Write function getValueOfProperty that takes in an object, and the name of a property on the object
 // return the value from the object that corresponds to the property
 
+function getValueOfProperty (obj, propertyName){
+
+return obj[propertyName]
+
+}
+
 
 // #13 Write a function called makeChatMessage that takes in a message and author as parameters
 // and returns an object with a message, author, and timestamp, that is
 // the current time as a Date object
 
+function makeChatMessage(message, author){
+
+	return object = { message: message,
+		 author: author,
+		 timestamp: new Date()
+
+	};
+
+}
+
 // #14 Create a function called coderTest that takes in an object that is a person. It looks to see if the person’s name is Jeremy and then changes the person object to have a property called lovesCode with a value of 10.  If their name is Brack set lovesCode to 0.  otherwise set lovesCode to 5.
+function coderTest(objectPerson){
+	if(objectPerson.name === 'Jeremy'){
+			objectPerson.lovesCode = 10;
+		}
+	else if(objectPerson.name === 'Brack') {
+		objectPerson.lovesCode = 0;
+	}
+	else objectPerson.lovesCode = 5;
+
+
+}
+//  the parenthesis is the Test.. if(objectPerson.name === 'Jeremy') The result of condition needs {}
 
 
 // #15 Create a function called outside that takes in a temperature (number), a humidity(number), and a cloudiness(number), in that order. Using the following to return the correct values
@@ -134,6 +182,30 @@ arr.splice()
     temperature over 80 or humidity over 50 or cloudiness over 50 - return "Hmm, probably not"
     Otherwise - return "I love outside"
 */
+function outside(t, h, c){
+
+	if(t>80){
+		if(h>40){
+				return "I'm all sweat";
+			}
+			else if(c<20 && h<40){
+				return "I'm literally in the desert"
+			}
+			return "Hmm, probably not"
+		}
+		else if(t<40 && c>60){return "I have icecicles"
+		}
+		else if(c>50){return "Hmm, probably not"
+		}
+		else if(h>50){return "Hmm, probably not"
+		}
+		return "I love outside";
+}
+
 
 // #16 Create a function called callerBack that takes in a function (holla) and a string parameter(back) and invokes it(holla) with the argument string(back) + ' back'."
-// example - If I call you with 'Give it' you should invoke holla with 'Give it back'
+// example - If I call you with 'Give it' youshould invoke holla with 'Give it back'
+
+function callerBack(cb, back){
+return	cb(back + ' back')
+}
